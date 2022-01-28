093c66ea4eb51b47ddfefca04397db7664720f37
jisu <jisuuoo96@gmail.com>
2022-01-28 20:22:30 +0900

    Test the membership storage method

---
+++ src/test/java/hello1/servlet1/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,49 @@
+package hello1.servlet1.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() { // 테스트가 다 끝나고 나면 저장소를 비워주는 기능을한다.
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("jisu", 20);
+
+        //when
+        Member savedMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("a", 10);
+        Member member2 = new Member("b", 15);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+
+    }
+}
