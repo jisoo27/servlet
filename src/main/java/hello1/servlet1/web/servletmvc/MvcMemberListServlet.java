f7504b0d4ec67c430019d8e2a44a563019d5755e
jisu <jisuuoo96@gmail.com>
2022-01-30 23:47:54 +0900

    Member list inquiry controller with mvc pattern

---
+++ src/main/java/hello1/servlet1/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,32 @@
+package hello1.servlet1.web.servletmvc;
+
+import hello1.servlet1.domain.member.Member;
+import hello1.servlet1.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MvcMemberListServlet.service");
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members); // request 객체를 사용해서 List<Member> members 를 모델에 보관했다
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+
+    }
+}
