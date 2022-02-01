76089a787f8b0a7b60280e30790a9ba119bc66d8
jisu <jisuuoo96@gmail.com>
2022-02-01 20:40:03 +0900

    Member registration form to apply the apply the front ControllerV2

---
+++ src/main/java/hello1/servlet1/web/frontcontroller/v2/MemberFormControllerV2.java
@@ -0,0 +1,15 @@
+package hello1.servlet1.web.frontcontroller.v2;
+
+import hello1.servlet1.web.frontcontroller.MyView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberFormControllerV2 implements ControllerV2{
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        return new MyView("/WEB-INF/views/new-form.jsp");
+    }
+}
